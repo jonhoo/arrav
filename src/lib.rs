243577a2d5e3e2519dff2c02cf894aad69508955
Jon Gjengset <jon@thesquareplanet.com>
2020-04-04 16:51:47 -0400

    Remove now-useless aux file
    Also fixes Windows build

--- src/lib.rs
+++ src/lib.rs
@@ -102,8 +102,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(feature = "std", deny(missing_debug_implementations))]
 
-#[doc(hidden)]
-pub mod aux;
 /// `Arrav` error types.
 pub mod errors;
 /// `Arrav` iterator types.
@@ -802,3 +800,24 @@ where
         self.ts.get_unchecked_mut(index)
     }
 }
+
+macro_rules! impl_sentinel_by_max {
+    ($t:tt) => {
+        impl Sentinel for $t {
+            const SENTINEL: Self = ::core::$t::MAX;
+        }
+    };
+}
+
+impl_sentinel_by_max!(u8);
+impl_sentinel_by_max!(i8);
+impl_sentinel_by_max!(u16);
+impl_sentinel_by_max!(i16);
+impl_sentinel_by_max!(u32);
+impl_sentinel_by_max!(i32);
+impl_sentinel_by_max!(u64);
+impl_sentinel_by_max!(i64);
+impl_sentinel_by_max!(u128);
+impl_sentinel_by_max!(i128);
+impl_sentinel_by_max!(usize);
+impl_sentinel_by_max!(isize);
